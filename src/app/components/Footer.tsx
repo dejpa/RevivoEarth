ac08bc8d480deedbfda0e89119052c3f3705300a
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2025-02-02 10:41:36 +0330

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,15 +4,15 @@ import { FaInstagram, FaLinkedin, FaXTwitter } from 'react-icons/fa6';
 export default function Footer() {
     return (
         <footer className="bg-zinc-200 text-green-950 p-8">
-            <div className="container mx-auto flex justify-between">
-                <div>
+            <div className="container mx-auto flex flex-wrap justify-between">
+                <div className='md-1/3'>
                     <h3 className="text-md md:text-lg font-bold">Contact Us</h3>
                     <p>Address: 123 Green St, Eco City</p>
                     <p>Phone: +123 456 7890</p>
                     <p>Email: info@biomulch.com</p>
                 </div>
-                <div>
-                <h3 className="text-md md:text-lg font-bold">Quick Links</h3>
+                <div className='md-1/3'>
+                    <h3 className="text-md md:text-lg font-bold">Quick Links</h3>
                     <ul>
                         <li><a href="/privacy" className="hover:text-white">Privacy Policy</a></li>
                         <li><a href="/terms" className="hover:text-white">Terms of Service</a></li>
@@ -20,7 +20,7 @@ export default function Footer() {
                         <li><a href="/contact" className="hover:text-white">Contact Us</a></li>
                     </ul>
                 </div>
-                <div>
+                <div className='md-1/3'>
                     <h3 className="text-md md:text-lg font-bold">Follow Us</h3>
                     <div className="flex space-x-4">
                         <a
