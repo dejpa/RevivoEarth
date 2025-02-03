679093408b2872abf6eff02850a32fb458e14d6c
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2025-02-03 11:19:21 +0330

    adding next-intl to project

---  next.config.js
+++  next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     i18n: {
       locales: ['en', 'fa', 'ar'], // زبان‌های پشتیبانی شده
       defaultLocale: 'en', // زبان پیش‌فرض
+      localeDetection: true, // تشخیص خودکار زبان مرورگر
     },
   };
   
