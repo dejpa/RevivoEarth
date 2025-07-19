c0c2812c1cce5e3793ae44387c62da60a9fbb25d
melina <m.niavarani@hotmail.com>
2025-07-19 11:40:34 +0330

    update home mobile view

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -26,17 +26,17 @@ export default function Home() {
           <div className="absolute inset-0 bg-black/50"></div>
         </div>
         <div className="container mx-auto text-center relative z-10">
-          <h1 className="text-5xl font-bold mb-4">
+          <h1 className="text-2xl md:text-5xl font-bold mb-4">
             {t("home.heroTitle")}
           </h1>
-          <p className="text-xl mb-8">
+          <p className="md:text-xl mb-8">
             {t("home.heroDescription")}
           </p>
           <div className="space-x-4 rtl:space-x-reverse">
-            <Link href="/about-us" className="bg-zinc-200 text-green-950 px-6 py-3 rounded-lg hover:bg-lime-600 hover:text-white transition duration-300">
+            <Link href="/about-us" className="text-sm md:text-lg bg-zinc-200 text-green-950 px-2 md:px-6 py-3 rounded-lg hover:bg-lime-600 hover:text-white transition duration-300">
               {t("home.learnMore")}
             </Link>
-            <a href="/contact" className="bg-transparent border border-white text-white px-6 py-3 rounded-lg hover:bg-white hover:text-green-950 transition duration-300">
+            <a href="/contact" className="text-sm md:text-lg bg-transparent border border-white text-white px-2 md:px-6 py-3 rounded-lg hover:bg-white hover:text-green-950 transition duration-300">
               {t("home.getQuote")}
             </a>
           </div>
@@ -49,7 +49,7 @@ export default function Home() {
           <h2 className="text-2xl md:text-4xl font-bold mb-8 text-green-700">
             {t("home.aboutUs")}
           </h2>
-          <p className="text-gray-600 mb-8 max-w-2xl mx-auto">
+          <p className="md:text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
             {t("home.aboutUsDescription")}
           </p>
         </div>
@@ -60,39 +60,39 @@ export default function Home() {
           <h2 className="text-2xl md:text-4xl font-bold mb-8 text-green-700">
             {t("home.mainTitle")}
           </h2>
-          <p className="text-gray-600 mb-8 max-w-2xl mx-auto">
+          <p className="md:text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
             {t("home.mainDescription")}
           </p>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {/* Vision */}
             <div className="p-6 bg-white rounded-lg shadow-md">
-              <FaLeaf className="text-green-700 h-12 w-12 mx-auto mb-4" />
-              <h3 className="text-xl font-semibold mb-2 text-gray-600">
+              <FaLeaf className="text-green-700 h-8 md:h-12 w-12 mx-auto mb-4" />
+              <h3 className="md:text-xl font-semibold mb-2 text-gray-600">
                 {t("home.ourVision")}
               </h3>
-              <p className="text-gray-600">
+              <p className="text-sm md:text-lg text-gray-600">
                 {t("home.ourVisionDescription")}
               </p>
             </div>
 
             {/* Mission */}
             <div className="p-6 bg-white rounded-lg shadow-md">
-              <FaCheckCircle className="text-green-700 h-12 w-12 mx-auto mb-4" />
-              <h3 className="text-xl font-semibold mb-2 text-gray-600">
+              <FaCheckCircle className="text-green-700 h-8 md:h-12 w-12 mx-auto mb-4" />
+              <h3 className="md:text-xl font-semibold mb-2 text-gray-600">
                 {t("home.ourMission")}
               </h3>
-              <p className="text-gray-600">
+              <p className="text-sm md:text-lg text-gray-600">
                 {t("home.ourMissionDescription")}
               </p>
             </div>
 
             {/* Values */}
             <div className="p-6 bg-white rounded-lg shadow-md">
-              <FaShieldAlt className="text-green-700 h-12 w-12 mx-auto mb-4" />
-              <h3 className="text-xl font-semibold mb-2 text-gray-600">
+              <FaShieldAlt className="text-green-700 h-8 md:h-12 w-12 mx-auto mb-4" />
+              <h3 className="md:text-xl font-semibold mb-2 text-gray-600">
                 {t("home.ourValues")}
               </h3>
-              <p className="text-gray-600">
+              <p className="text-sm md:text-lg text-gray-600">
                 {t("home.ourValuesIntegrityDescription")}
               </p>
             </div>
@@ -104,15 +104,15 @@ export default function Home() {
       {/* Call to Action */}
       <section className="py-16 bg-green-950 text-white">
         <div className="container mx-auto px-4 text-center">
-          <h2 className="text-2xl md:text-4xl font-bold mb-8">
+          <h2 className="text-xl md:text-4xl font-bold mb-8">
             {t("home.makeChange")}
           </h2>
-          <p className="text-xl mb-8">
+          <p className="text-sm md:text-xl mb-8">
             {t("home.contactUsDescription")}
           </p>
           <a
             href="/contact"
-            className="bg-white text-green-950 px-6 py-3 rounded-lg hover:bg-gray-100 transition duration-300"
+            className="bg-white text-green-950 text-sm md:text-lg px-2 md:px-6 py-3 rounded-lg hover:bg-gray-100 transition duration-300"
           >
             {t("home.contactUs")}
           </a>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -20,22 +20,22 @@ export default function Footer() {
     return (
         <footer className="bg-zinc-200 text-green-950 p-8">
             <div className="container mx-auto flex flex-wrap justify-between">
-                <div className='md-1/3'>
-                    <h3 className="text-md md:text-lg font-bold">{t("footer.contactUs")}</h3>
+                <div className='md-1/3 text-sm md:text-lg'>
+                    <h3 className=" font-bold">{t("footer.contactUs")}</h3>
                     {/* <p>{t("footer.address")}: 123 Green St, Eco City</p>
                     <p>{t("footer.phone")}: {t("footer.phoneDetails")}</p> */}
                     <p>{t("footer.email")}: {t("footer.emailDetails")}</p>
                 </div>
-                <div className='md-1/3'>
-                    <h3 className="text-md md:text-lg font-bold">{t("footer.quickLinks")}</h3>
-                    <ul>
+                <div className='text-sm md:text-lg md-1/3'>
+                    <h3 className="font-bold">{t("footer.quickLinks")}</h3>
+                    <ul >
                         <li><a href="/privacy" className="hover:text-white">{t("footer.privacyPolicy")}</a></li>
                         {/* <li><a href="/terms" className="hover:text-white">Terms of Service</a></li> */}
                         <li><a href="/about" className="hover:text-white">{t("footer.aboutUs")}</a></li>
                         <li><a href="/contact" className="hover:text-white">{t("footer.contactUs")}</a></li>
                     </ul>
                 </div>
-                <div className='md-1/3'>
+                <div className='text-sm md:text-lg md-1/3'>
                     <h3 className="text-md md:text-lg font-bold">{t("footer.followUs")}</h3>
                     <div className="flex space-x-4 rtl:space-x-reverse">
                         <a
@@ -44,7 +44,7 @@ export default function Footer() {
                         rel="noopener noreferrer"
                         className="hover:text-white"
                         >
-                            <FaInstagram className="h-6 w-6" />
+                            <FaInstagram className="h-4 md:h-6 w-6" />
                         </a>
                         <a
                         href="https://www.linkedin.com/company/yourcompany"
@@ -52,7 +52,7 @@ export default function Footer() {
                         rel="noopener noreferrer"
                         className="hover:text-white"
                         >
-                        <FaLinkedin className="h-6 w-6" />
+                            <FaLinkedin className="h-4 md:h-6 w-6" />
                         </a>
                         <a
                         href="https://twitter.com/yourcompany"
@@ -60,12 +60,12 @@ export default function Footer() {
                         rel="noopener noreferrer"
                         className="hover:text-white"
                         >
-                        <FaXTwitter className="h-6 w-6" />
+                            <FaXTwitter className="h-4 md:h-6 w-6" />
                         </a>
                     </div>
                 </div>
             </div>
-            <div className="text-center mt-8">
+            <div className="text-sm md:text-lg text-center mt-8">
                 <p>© 2025 RevivoEarth. All rights reserved.</p>
             </div>
         </footer>
